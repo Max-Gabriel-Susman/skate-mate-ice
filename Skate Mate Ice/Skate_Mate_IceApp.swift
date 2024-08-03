2ed82aa8bf5b92f07e55c185d3c929dd58e14532
Gabe Susman <gabe.susman@gmail.com>
2024-08-03 16:26:58 -0600

    Initial Commit

---
+++ Skate Mate Ice/Skate_Mate_IceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skate_Mate_IceApp.swift
+//  Skate Mate Ice
+//
+//  Created by Max Gabriel Susman on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Skate_Mate_IceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
