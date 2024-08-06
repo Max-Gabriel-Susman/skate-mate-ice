eca7dcbcabe91f5caeb6e880fc5fa4b9a3267202
Gabe Susman <gabe.susman@gmail.com>
2024-08-06 15:15:38 -0600

    it builds, but still neeeds some work

--- Skate Mate Ice/ContentView.swift
+++ Skate Mate Ice/ContentView.swift
@@ -1,26 +1,26 @@
-//
-//  ContentView.swift
-//  Skate Mate Ice
-//
-//  Created by Max Gabriel Susman on 8/3/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var bleManager = BLEManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(bleManager.peripherals, id: \.identifier) { peripheral in
+                VStack(alignment: .leading) {
+                    Text(peripheral.name ?? "Unknown")
+                        .font(.headline)
+                    Text(peripheral.identifier.uuidString)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                .onTapGesture {
+                    bleManager.connect(to: peripheral)
+                }
+            }
+            .navigationTitle("BLE Devices")
+            .onAppear {
+                bleManager.startScanning()
+            }
         }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
--- Skate Mate Ice/Skate_Mate_IceApp.swift
+++ Skate Mate Ice/Skate_Mate_IceApp.swift
@@ -91,28 +91,3 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeriph
         }
     }
 }
-
-struct ContentView: View {
-    @StateObject private var bleManager = BLEManager()
-    
-    var body: some View {
-        NavigationView {
-            List(bleManager.peripherals, id: \.identifier) { peripheral in
-                VStack(alignment: .leading) {
-                    Text(peripheral.name ?? "Unknown")
-                        .font(.headline)
-                    Text(peripheral.identifier.uuidString)
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-                .onTapGesture {
-                    bleManager.connect(to: peripheral)
-                }
-            }
-            .navigationTitle("BLE Devices")
-            .onAppear {
-                bleManager.startScanning()
-            }
-        }
-    }
-}
